35b415e2744c915459a485ab1c3098aa06ebaa1b
Robyn H Richardson <robynhrich@icloud.com>
2024-01-26 18:33:43 -0500

    used jQuery to  check which button is pressed

--- README.md
+++ README.md
@@ -5,3 +5,14 @@ Create JS file
 Add/Link JavaScript and jQuery
 
 Create function to generate new random numbers between 0 and 3
+Create an array to hold button color sequence
+Create a pattern with the numbers and buttons
+
+Flash Animate random chosen color
+play sound for color selected
+
+Check which button is pressed
+
+Add sounds to button clicks
+
+Add animations to user clicks
--- game.js
+++ game.js
@@ -1,5 +1,30 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+});
+
 function nextSequence() {
   var randomNumber = Math.random();
   randomNumber = Math.floor(randomNumber * 4);
-  console.log(randomNumber);
+
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  playSound(randomChosenColor);
+
+  function playSound(name) {
+    $("#" + randomChosenColor)
+      .fadeOut(100)
+      .fadeIn(100)
+      .fadeOut(100)
+      .fadeIn(100);
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
 }
